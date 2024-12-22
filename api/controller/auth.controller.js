8ffb3d1fe91ba23c9881a02110ce56120835dbee
Tenjing Sherpa <10jingsherpa@gmail.com>
2024-12-22 11:58:32 +0545

    create signup API route

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+ 
+    // Destructure the request body
+    const { username, email, password } = req.body;
+
+    
+    // Hash the password
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, email, password: hashedPassword });
+    try{
+         // Save the user to the database
+    await newUser.save();
+
+    // Respond with success
+    res.status(201).json({ message: "User created successfully!" });
+  
+    }catch(error){
+        res.status(500).json(error.message);
+    }
+   
+};
--- api/index.js
+++ api/index.js
@@ -1,32 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-// MongoDB Connection
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO
+  )
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.error('Failed to connect to MongoDB', err.message);
+    console.error("Failed to connect to MongoDB:", err.message);
   });
 
-// Initialize Express
 const app = express();
 
-// Middleware
+// Middleware for parsing JSON requests
 app.use(express.json());
 
-// Routes
-app.use('/api/user', userRouter);
+// Use the auth router
+app.use("/api/auth", authRouter);
 
-// Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
