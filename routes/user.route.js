8898a910d87f9cc9720e11a382cd8c71454340e3
Tenjing Sherpa <10jingsherpa@gmail.com>
2025-04-15 23:12:55 +0545

    add user management API endpoints

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,14 +4,12 @@ import {
   updateUser,
   deleteUser,
   getAllUser,
+  getUserById,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/", test);
-router.post("/update/:id", verifyToken, updateUser);
-router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/getall", getAllUser);
+router.get("/", test).post("/update/:id", verifyToken, updateUser).delete("/delete/:id", verifyToken, deleteUser).get("/getall", getAllUser).get("/getbyid/:id",getUserById)
 
 export default router;
