2209cec6c46f0012649a90fc5fa8b44d357a5b4a
Tenjing Sherpa <10jingsherpa@gmail.com>
2025-04-15 18:18:25 +0545

    add endpoint to get user by ID

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -80,3 +80,21 @@ export const getAllUser = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
